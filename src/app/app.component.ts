86e35c2884697c59515c457ef9c5743c17e83f3d
manavpandya <pandya.manav@gmail.com>
2018-10-04 18:33:33 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'publishangulartoheroku';
+  title = 'C#Corner - Publish Angular app to Heroku';
 }
